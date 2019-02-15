34126ca2e03295cb8ac96639b0c1f59af5791edc
Harry Twan <gua@desgard.com>
2019-02-15 09:45:29 +0800

    切换成本地 Pods 开发
    Signed-off-by: Harry Twan <gua@desgard.com>

---
+++ Shorthair.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+  s.name         = "Shorthair"
+  s.version      = "0.1.0"
+  s.summary      = "The issue and pull request UI framework"
+  s.description  = <<-DESC
+  The issue and pull request UI framework
+                   DESC
+  s.homepage     = "http://desgard.com"
+  s.license      = "GPL-3.0"
+  s.author       = { "Harry Twan" => "gua@desgard.com" }
+  s.source       = { :git => "https://github.com/Sepicat/Shorthair.git", :tag => "0.1.0" }
+  s.source_files  =  "Shorthair/Shorthair/**/*.{swift}"
+  s.ios.deployment_target = '10.0'
+  s.dependency 'SnapKit', '~> 4.2.0'
+  s.dependency 'YYText', '~> 1.0.4'
+end
--- Shorthair/Shorthair/Issue/View/ElementCell/ShorLeftBubbleCell.swift
+++ Shorthair/Shorthair/Issue/View/ElementCell/ShorLeftBubbleCell.swift
@@ -11,13 +11,36 @@ import SnapKit
 
 public class ShorLeftBubbleCell: UITableViewCell {
     
+    private var bakView: UIView = {
+        var view = UIView()
+        view.backgroundColor = UIColor.gray
+        return view
+    }()
+    
     lazy private var avatarImageView: UIImageView = {
         var imageView = UIImageView(image: UIImage(named: "logo-icon.png"))
         imageView.layer.masksToBounds = true
         imageView.layer.cornerRadius = 20
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         imageView.backgroundColor = .white
         return imageView
+    }()
+    
+    lazy private var loginLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Desgard"
+        label.textColor = UIColor.lightGray
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    lazy private var bubbleView: UIView = {
+        var view = UIView()
+        view.backgroundColor = UIColor.white
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 8
+        return view
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -27,14 +50,37 @@ public class ShorLeftBubbleCell: UITableViewCell {
     }
     
     private func initialViews() {
-        addSubview(avatarImageView)
+        addSubview(bakView)
+        bakView.addSubview(avatarImageView)
+        bakView.addSubview(loginLabel)
+        bakView.addSubview(bubbleView)
     }
     
     private func initialLayouts() {
+        
+        bakView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
         avatarImageView.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(20)
             make.width.height.equalTo(40)
             make.top.equalToSuperview().offset(10)
+            make.bottom.lessThanOrEqualToSuperview().offset(-20)
+        }
+        
+        loginLabel.snp.makeConstraints { make in
+            make.top.equalTo(avatarImageView)
+            make.left.equalTo(avatarImageView.snp.right).offset(10)
+        }
+        
+        bubbleView.snp.makeConstraints { make in
+            make.left.equalTo(loginLabel)
+            make.top.equalTo(loginLabel.snp.bottom).offset(6)
+            make.right.equalToSuperview().offset(-20)
+            
+            let height = Float.random(in: 40..<200)
+            make.height.greaterThanOrEqualTo(height)
             make.bottom.lessThanOrEqualToSuperview().offset(-20)
         }
     }
